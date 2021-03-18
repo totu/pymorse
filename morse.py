f98118fbf4b7ecb9ff36e9d431561c2704bbe8bd
totu <topi@topituulensuu.com>
2021-03-18 12:56:28 +0200

    initial sound implementation

--- morse.py
+++ morse.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 from libmorse import MORSE_ALPHABET
+import os
+from time import sleep
+
+if os.name == "nt":
+    import winsound
+    WINDOWS = True
+else:
+    WINDOWS = False
+
+FREQ = 1000
+UNIT = 110
 
 
 def convert_text_to_morse(string):
@@ -26,3 +37,27 @@ def convert_morse_to_text(string, dash="-", dot="·"):
 
     return out.strip()
 
+def play(string):
+    dash = "-"
+    out = []
+    for word in string.split("  "):
+        for character in word.split(" "):
+            for c in character:
+                duration = UNIT
+                if c == dash:
+                    duration = duration * 3
+                out.append((True, duration))
+                out.append((False, UNIT))
+            out.append((False, UNIT * 3))
+        out.append((False, UNIT * 7))
+
+    for play, duration in out:
+        if play: 
+            winsound.Beep(FREQ, duration)
+        else:
+            sleep(duration//1000)
+
+
+if __name__ == "__main__":
+    x = convert_text_to_morse("tämä on testi")
+    play(x)
