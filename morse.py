bd0a8c2de9cb9d587124e36c4aebdb28b0482c1c
totu <topi@topituulensuu.com>
2021-03-19 19:56:17 +0200

    add wav file creation and playing

--- morse.py
+++ morse.py
@@ -2,15 +2,13 @@
 from libmorse import MORSE_ALPHABET
 import os
 from time import sleep
+import wave
+import struct
+import tempfile
+from playsound import playsound
 
-if os.name == "nt":
-    import winsound
-    WINDOWS = True
-else:
-    WINDOWS = False
-
-FREQ = 1000
-UNIT = 110
+FREQ = 2000
+UNIT = 90
 
 
 def convert_text_to_morse(string):
@@ -20,7 +18,7 @@ def convert_text_to_morse(string):
         if character in MORSE_ALPHABET:
             out.append(MORSE_ALPHABET[character])
     # TODO: This needs more thought
-    return " ".join(out).replace(" "*3, " "*2)
+    return " ".join(out).replace(" " * 3, " " * 2)
 
 
 def convert_morse_to_text(string, dash="-", dot="·"):
@@ -37,7 +35,8 @@ def convert_morse_to_text(string, dash="-", dot="·"):
 
     return out.strip()
 
-def play(string):
+
+def _create_output(string):
     dash = "-"
     out = []
     for word in string.split("  "):
@@ -53,25 +52,53 @@ def play(string):
 
     return out
 
-if __name__ == "__main__":
-    x = convert_text_to_morse("tämä on testi")
-    out = play(x)
-    import wave, struct, random
-    obj = wave.open("test.wav", "w")
+
+def _make_wave(out, filename):
+    obj = wave.open(f"{filename}", "w")
     obj.setnchannels(1)
     obj.setsampwidth(2)
-    obj.setframerate(1000)
+    obj.setframerate(FREQ)
+
     for play, duration in out:
-        duration = duration 
-        data = struct.pack('<h', play * 15000)
+        duration = duration
         for _ in range(duration):
-            obj.writeframesraw( data )
-    # for play, duration in out:
-    #     data = struct.pack("<h", 100)
-    #     data = struct.pack("<h", 100)
-    #     data = struct.pack("<h", 100)
-    #     data = struct.pack("<h", 100)
-    #     data = struct.pack("<h", 100)
-    #     obj.writeframesraw(data)
-    
+            data = struct.pack("<h", play * 0x7FFF)
+            obj.writeframesraw(data)
+            data = struct.pack("<h", play * -0x7FFF)
+            obj.writeframesraw(data)
+
     obj.close()
+
+
+def play_string(string):
+    morse = convert_text_to_morse(string)
+    play_morse(morse)
+
+
+def play_morse(morse, filename=None, play=True):
+    out = _create_output(morse)
+    context = tempfile.NamedTemporaryFile()
+
+    # Create correct context
+    if filename:
+        context = open(filename, "wb")
+    else:
+        filename = context.name
+
+    with context as ctx:
+        _make_wave(out, filename)
+        if play:
+            playsound(filename)
+
+
+def create_wav(string, filename, morse=False):
+    if not morse:
+        string = convert_text_to_morse(string)
+
+    play_morse(string, filename, play=False)
+
+
+if __name__ == "__main__":
+    # play_string("sos")
+    morse = convert_text_to_morse("sos")
+    create_wav("sos", "sos.wav", morse=False)
